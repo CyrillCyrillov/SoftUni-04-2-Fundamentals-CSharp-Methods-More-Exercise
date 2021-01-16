32f5967ccc57f7201e51a907f90b809033854d8c
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:46:52 +0200

    Add files via upload

---
+++ Task04_Tribonacci_Sequence/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task04_Tribonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number == 1)
+                {
+                    Console.WriteLine(number);
+                }
+            else if(number == 2)
+                {
+                    Console.WriteLine("1 1");
+                }
+             else
+            {
+                int numberOne = 0;
+                int numberTwo = 1;
+                int numberThree = 1;
+
+                Console.Write("1 1 ");
+
+                for (int i = 3; i <= number; i++)
+                {
+                    int nextResult = NextResult(numberOne, numberTwo, numberThree);
+                    Console.Write($"{nextResult} ");
+                    numberOne = numberTwo;
+                    numberTwo = numberThree;
+                    numberThree = nextResult;
+                }
+            }
+            
+            //Console.WriteLine("Hello World!");
+        }
+
+        private static int NextResult(int numberOne, int numberTwo, int numberThree)
+        {
+            int result = numberOne + numberTwo + numberThree;
+            return result;
+        }
+    }
+}
