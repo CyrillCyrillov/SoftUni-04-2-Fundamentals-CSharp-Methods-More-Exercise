dab1a9ea191af84bc3e32ced8668be902d0464b5
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:45:15 +0200

    Add files via upload

---
+++ Task02_Center_Point/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task02_Center_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            CloserToCenter(x1, y1, x2, y2);
+        }
+
+        private static void CloserToCenter(double x1, double y1, double x2, double y2)
+        {
+            if (Math.Sqrt(x1 * x1 + y1 * y1) <= Math.Sqrt(x2 * x2 + y2 * y2))
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+        }
+    }
+}
