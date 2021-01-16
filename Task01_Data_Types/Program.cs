8a6ecce175b4d32b7caf16b7664ed6a645b5d9b1
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-16 20:44:20 +0200

    Add files via upload

---
+++ Task01_Data_Types/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task01_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string numberString = Console.ReadLine();
+
+            string result = CheckAndCalculate(type, numberString);
+            Console.WriteLine(result);
+        }
+
+        private static string CheckAndCalculate(string type, string numberString)
+        {
+            string result = string.Empty;
+            if(type == "int")
+            {
+                result = (int.Parse(numberString) * 2).ToString();
+            }
+            else if (type == "real")
+            {
+                result = ((double.Parse(numberString) * 1.5)).ToString("0.00");
+            }
+            else
+            {
+                result = '$' + numberString + '$';
+            }
+
+            return result;
+        }
+    }
+}
